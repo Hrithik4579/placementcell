099385e5681c88fce13fac50549a64cf040a7537
Dhruv Gupta <dhruv.gupta1873@gmail.com>
2024-02-23 00:13:23 +0530

    added admin db

---
+++ server/src/models/admin.model.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from "mongoose";
+
+const adminSchema = new Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },      
+    },
+    {
+        timestamps: true
+    }
+)
+
+adminSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+adminSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            userId: this.enrollmentNumber,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+adminSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const Admin = mongoose.model('Admin', adminSchema);
