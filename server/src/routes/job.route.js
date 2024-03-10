f00a779fa59149d9d2e3dba7b371d7ac016f0915
Dhruv Gupta <dhruv.gupta1873@gmail.com>
2024-03-10 14:03:16 +0530

    added check, if student or admin is logged in for fetchalljobs

--- server/src/routes/job.route.js
+++ server/src/routes/job.route.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { createJob, deleteJob, makeJobInactive, fetchAllJobs } from "../controllers/job.controller.js";
 import { verifyAdminJWT } from "../middlewares/adminAuth.middleware.js";
 import { verifyStudentJWT } from "../middlewares/studentAuth.middleware.js"
+import { verifyLoginJWT } from "../middlewares/loggedInAuth.middleware.js"; 
 
 const router = Router()
 
 router.get("/createjob", verifyAdminJWT, createJob)
 router.get("/deletejob", verifyAdminJWT, deleteJob)
 router.get("/makejobinactive", verifyAdminJWT, makeJobInactive)
-router.get("/fetchalljobs", verifyStudentJWT || verifyAdminJWT, fetchAllJobs)
+router.get("/fetchalljobs", verifyLoginJWT, fetchAllJobs)
 
 export default router
