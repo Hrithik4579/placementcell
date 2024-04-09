9cd7b6d645c6079e8aba92473cbee55c35f8ac95
Hrithik4579 <hbhagat2003@gmail.com>
2024-04-10 01:11:28 +0530

    m

---
+++ client/src/components/Addstudent.js
@@ -0,0 +1,48 @@
+import "./login.css"
+import React from 'react'
+import Anavbar from './Anavbar'
+import Asidebar from "./Asidebar"
+export default function Addstudent() {
+  return (
+    <div>
+        <Anavbar/>
+        <div className='mt-3 nav'>
+            <h2><u>Enter student details below:</u></h2>
+        </div>
+      <form className="admin_form">
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Student Name</label>
+    <input type="text" className="form-control" id="name1"/>
+  </div>
+  <div className='mb-3'>
+    <label for='enrol' className='form-label'>Enrollment Number:</label>
+    <input type='number' className='form-control'/>
+  </div>
+  <div className='mb-3'>
+    <label for='email' className='form-label'>Email:</label>
+    <input type='email' className='form-control'/>
+  </div>
+  <div className='mb-3'>
+    <label for='branch' className='form-label'>Branch:</label>
+    <input type='text' className='form-control'/>
+  </div>
+  <div className='mb-3'>
+    <label for='gradyear' className='form-label'>Graduation Year:</label>
+    <input type='number' className='form-control'/>
+  </div>
+  <div className='mb-3'>
+    <label for='cgpa' className='form-label'>CGPA:</label>
+    <input type='number' className='form-control'/>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  
+  <div>
+  <button type="submit" className="btn btn-dark">Add Student</button>
+  </div>
+</form>
+    </div>
+  )
+}
---
+++ client/src/components/Companyitem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./login.css"
+export default function Companyitem(props) {
+  return (
+    <div className='company_card'>
+      <div className="card">
+  <div className="card-header">
+    {props.cname}
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">{props.post}</h5>
+    <p className="card-text">{props.ctc}</p>
+    <Link to="#" className="btn btn-primary">View Opportunity</Link>
+  </div>
+</div>
+    </div>
+  )
+}
