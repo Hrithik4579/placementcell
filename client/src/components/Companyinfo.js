22d50b57f28add8b17869bb11001f512a885e897
kushagra10bansal <115881616+kushagra10bansal@users.noreply.github.com>
2024-04-28 00:02:31 +0530

    2nd time commitiung

--- client/src/components/Companyinfo.js
+++ client/src/components/Companyinfo.js
@@ -1,57 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import Anavbar from './Anavbar'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import Anavbar from "./Anavbar";
+import { useParams } from "react-router-dom";
+import "./Companyinfo.css";
 
 export default function Companyinfo(props) {
-   const {companyId}=useParams();
-   const [company,setCompany]=useState({});
-   useEffect(()=>{
-    const fetchCompany=async()=>{
-      try{
-        const response=await fetch(`http://localhost:8000/api/admin/job/${companyId}`,{
-          method: "GET",
-          credentials: 'include',
-          header:{
-            "Content-Type":"application/json",
+  const { companyId } = useParams();
+  const [company, setCompany] = useState({});
+  useEffect(() => {
+    const fetchCompany = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/admin/job/${companyId}`,
+          {
+            method: "GET",
+            credentials: "include",
+            header: {
+              "Content-Type": "application/json",
+            },
           }
-        });
-        const json=await response.json();
-        if(json.success){
+        );
+        const json = await response.json();
+        if (json.success) {
           setCompany(json.data);
-        }
-        else{
+        } else {
           console.log("fetching unsuccessful");
         }
-      }
-      catch(error){
-        console.log("error fetching company",error);
+      } catch (error) {
+        console.log("error fetching company", error);
       }
     };
     fetchCompany();
-  },[companyId]);
-  const formatRegister=(dateString)=>{
-    return  new Date(dateString).toLocaleDateString();
-  }
+  }, [companyId]);
+  const formatRegister = (dateString) => {
+    return new Date(dateString).toLocaleDateString();
+  };
   return (
     <div>
-      <Anavbar/>
-      <h1 className='text-center'><b><u>Company Details</u></b></h1>
+      <Anavbar />
+      <div className="container-fluid hello">
+      <h1 className="text-center">
+        <b>
+          Company Details
+        </b>
+      </h1>
 
-      {Object.keys(company).length>0?(
-      <div className='mt-5'>
-        <span>Company name: {company.companyName}</span>
-        <hr/>
-        <span className='display-8'>Batch: {company.batch}</span><hr/>
-        <span className='display-8'>CGPA: {company.cgpa} </span><hr/>
-        <span className='display-8'>Branch: {company.branches}</span><hr/>
-        <span className='display-8'>Place of posting: {company.location}</span><hr/>
-        <span className='display-8'>Role: {company.role}</span><hr/>
-        <span className='display-8'>Stipend: {company.salary}</span><hr/>
-        <span className='display-8'>Category: {company.type}</span><hr/>
-        <span className='display-8'>Registration last date: {formatRegister(company.registerBy)}</span><hr/>  
-      </div>
-      ):(<p>loading...</p>
+      {Object.keys(company).length > 0 ? (
+        <div className="mt-5">
+          <span className="cname">Company name: {company.companyName}</span>
+          <hr />
+          <span className="display-8">Batch: {company.batch}</span>
+          <hr />
+          <span className="display-8">CGPA: {company.cgpa} </span>
+          <hr />
+          <span className="display-8">Branch: {company.branches}</span>
+          <hr />
+          <span className="display-8">
+            Place of posting: {company.location}
+          </span>
+          <hr />
+          <span className="display-8">Role: {company.role}</span>
+          <hr />
+          <span className="display-8">Stipend: {company.salary}</span>
+          <hr />
+          <span className="display-8">Category: {company.type}</span>
+          <hr />
+          <span className="display-8">
+            Registration last date: {formatRegister(company.registerBy)}
+          </span>
+          <hr />
+        </div>
+      ) : (
+        <p>loading...</p>
       )}
       </div>
-  )
+    </div>
+  );
 }
--- client/src/components/Companyitem.js
+++ client/src/components/Companyitem.js
@@ -1,38 +1,76 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import "./login.css"
-import Companyinfo from './Companyinfo'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
+import "./Companyitem.css";
+import Companyinfo from "./Companyinfo";
 export default function Companyitem(props) {
-  const {articles} = props;
-  const [art,setart]=useState(articles);
-    const deleteArticle= async(id)=>{
-      const response =  await fetch(`http://localhost:8000/api/job/${id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            "auth-token": JSON.parse(localStorage.getItem('token'))
-          }
-        });
-        const json=response.json();
-      const newarticles=art.filter((article)=>{return article._id!==id});
-      setart(newarticles);
-  }
-  const Deleteartcle=()=>{
+  const { articles } = props;
+  const [art, setart] = useState(articles);
+  const deleteArticle = async (id) => {
+    const response = await fetch(`http://localhost:8000/api/job/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": JSON.parse(localStorage.getItem("token")),
+      },
+    });
+    const json = response.json();
+    const newarticles = art.filter((article) => {
+      return article._id !== id;
+    });
+    setart(newarticles);
+  };
+  const Deleteartcle = () => {
     deleteArticle(art._id);
-  }
+  };
   return (
-    <div className='company_card'>
-      <div className="card">
-        <div className="card-header">
-          {props.post}
+    <div className="company_card">
+      {/* <div class="container mt-5 mb-3"> */}
+      {/* <div class="row"> */}
+      {/* <div class="col-md-4"> */}
+      <div class="card p-3 mb-2">
+        <div class="d-flex justify-content-between">
+          <div class="d-flex flex-row align-items-center">
+            <div class="icon">
+              {" "}
+              <i class="bx bxl-mailchimp"></i>{" "}
+            </div>
+            <div class="ms-2 c-details">
+              <h6 class="mb-0">{props.cname}</h6>
+            </div>
+          </div>
+          <div class="badge">
+            {" "}
+            <span>Design</span>{" "}
+          </div>
         </div>
-        <div className="card-body">
-          <h5 className="card-title">{props.cname}</h5>
-          <p className="card-text">{props.ctc}</p>
-          <Link to={`/company/${props.id}`} className="btn btn-primary">view opportunity</Link>
-
+        <div class="mt-5">
+          <h3 class="heading">{props.post}</h3>
+          <h3>{props.ctc}</h3>
+        </div>
+        <div className="flex">
+        <Link to={`/company/${props.id}`} className="btn btn-primary button-33 ">
+          view 
+        </Link>
+        
         </div>
       </div>
+      {/* </div> */}
+      {/* </div> */}
+      {/* </div> */}
+
+      {/* <div className="card text-white">
+        <div className="card-header">{props.cname}</div>
+        <div className="card-body">
+          <h5 className="card-title">{props.post}</h5>
+          <h5>
+            <p className="card-text  mb-3">{props.ctc}</p>
+          </h5>
+          <Link to={`/company/${props.id}`} className="btn btn-primary ">
+            view opportunity
+          </Link>
+        </div>
+      </div> */}
     </div>
-  )
+  );
 }
