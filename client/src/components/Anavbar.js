8e18797890731b5bcb0fd6c5fbf388050dcf24f8
kushagra10bansal <115881616+kushagra10bansal@users.noreply.github.com>
2024-05-02 13:40:54 +0530

    home page

--- client/src/components/Anavbar.js
+++ client/src/components/Anavbar.js
@@ -27,12 +27,12 @@ export default function Anavbar() {
   const location = useLocation();
   return (
     <div>
-      {/* <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <div class="jiitlogo">
+          <div className="jiitlogo">
             <i className="bx bxl-mailchimp"></i>
           </div>
-          <Link className="navbar-brand bebas-neue-regular " to="/home">
+          <Link className="navbar-brand bebas-neue-regular" to="/home">
             JIIT T&P
           </Link>
           <button
@@ -51,123 +51,77 @@ export default function Anavbar() {
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav">
-              <li className="nav-item {`nav-item ${location.pathname === '/home' ? 'active' : ''}`} ">
+              <li
+                className={`nav-item  ${
+                  location.pathname === "/home" ? "active" : ""
+                }`}
+              >
                 <Link className="nav-link" aria-current="page" to="/home">
                   Home
                 </Link>
               </li>
-              <li className="nav-item ms-lg-0 {`nav-item ${location.pathname === '/home' ? 'active' : ''}`}">
+              <li
+                className={`nav-item ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}
+              >
                 <Link className="nav-link" to="/about">
                   About
                 </Link>
               </li>
-              <li className="nav-item ms-lg-0 {`nav-item ${location.pathname === '/home' ? 'active' : ''}`}">
+              <li
+                className={`nav-item ${
+                  location.pathname === "/addstudent" ? "active" : ""
+                }`}
+              >
                 <Link className="nav-link" to="/addstudent">
                   Add Student
                 </Link>
               </li>
-              <li className="nav-item ms-lg-0 {`nav-item ${location.pathname === '/home' ? 'active' : ''}`}">
-                <Link className="nav-link " to="/addcompany">
+              <li
+                className={`nav-item ${
+                  location.pathname === "/addcompany" ? "active" : ""
+                }`}
+              >
+                <Link className="nav-link" to="/addcompany">
                   Add Company
                 </Link>
               </li>
-              <li className="nav-item ms-lg-0">
+              <li
+                className={`nav-item ${
+                  location.pathname === "/ablog" ? "active" : ""
+                }`}
+              >
                 <Link className="nav-link" to="/ablog">
                   Add Blog
                 </Link>
               </li>
-              <li className="nav-item ms-lg-0">
+              <li
+                className={`nav-item ${
+                  location.pathname === "/vblog" ? "active" : ""
+                }`}
+              >
                 <Link className="nav-link" to="/vblog">
                   View Blogs
                 </Link>
               </li>
-              <li id="logout">
-                <Link to="/logout">
-                  <button
-                    onClick={handleClick}
-                    type="button"
-                    className="btn btn-secondary"
-                  >
-                    Logout
-                  </button>
-                </Link>
-              </li>
             </ul>
           </div>
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item" id="logout">
+              <Link to="/logout">
+                <button
+                  onClick={handleClick}
+                  type="button"
+                  className="btn btn-secondary btn1"
+                >
+                  Logout
+                </button>
+              </Link>
+            </li>
+          </div>
         </div>
-      </nav> */}
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <div className="jiitlogo">
-      <i className="bx bxl-mailchimp"></i>
-    </div>
-    <Link className="navbar-brand bebas-neue-regular" to="/home">
-      JIIT T&P
-    </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div
-      className="collapse navbar-collapse justify-content-center"
-      id="navbarSupportedContent"
-    >
-      <ul className="navbar-nav">
-        <li className={`nav-item  ${location.pathname === '/home' ? 'active' : ''}`}>
-          <Link className="nav-link" aria-current="page" to="/home">
-            Home
-          </Link>
-        </li>
-        <li className={`nav-item ${location.pathname === '/about' ? 'active' : ''}`}>
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </li>
-        <li className={`nav-item ${location.pathname === '/addstudent' ? 'active' : ''}`}>
-          <Link className="nav-link" to="/addstudent">
-            Add Student
-          </Link>
-        </li>
-        <li className={`nav-item ${location.pathname === '/addcompany' ? 'active' : ''}`}>
-          <Link className="nav-link" to="/addcompany">
-            Add Company
-          </Link>
-        </li>
-        <li className={`nav-item ${location.pathname === '/ablog' ? 'active' : ''}`}>
-          <Link className="nav-link" to="/ablog">
-            Add Blog
-          </Link>
-        </li>
-        <li className={`nav-item ${location.pathname === '/vblog' ? 'active' : ''}`}>
-          <Link className="nav-link" to="/vblog">
-            View Blogs
-          </Link>
-        </li>
-      </ul>
-    </div>
-    <div className="navbar-nav ml-auto">
-      <li className="nav-item" id="logout">
-        <Link to="/logout">
-          <button
-            onClick={handleClick}
-            type="button"
-            className="btn btn-secondary btn1"
-          >
-            Logout
-          </button>
-        </Link>
-      </li>
-    </div>
-  </div>
-</nav>
-
+      </nav>
     </div>
   );
 }
